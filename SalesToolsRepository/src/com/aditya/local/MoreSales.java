cab38ce4c9ffe6c8fc8b5fcebd74a795ecd7f4c8
Aditya Agarwal <skymanaditya1@gmail.com>
2015-05-25 13:05:36 +0530

    Initial checking of sales tools

---
+++ SalesToolsRepository/src/com/aditya/local/MoreSales.java
@@ -0,0 +1,8 @@
+package com.aditya.local;
+
+public class MoreSales {
+
+	public void printSalesData(){
+		System.out.println("This just prints more data to be implemented");
+	}
+}
---
+++ SalesToolsRepository/src/com/aditya/local/SalesInformation.java
@@ -0,0 +1,10 @@
+package com.aditya.local;
+
+public class SalesInformation {
+
+	public static void main(String[] args){
+		MoreSales sales = new MoreSales();
+		sales.printSalesData();
+		System.out.println("Just contains some information about sales");
+	}
+}
