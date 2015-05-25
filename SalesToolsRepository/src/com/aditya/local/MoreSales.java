aca564237c01ff5d130c3ebf843bfd51705d5f7e
Syed Munawwar Quadri <syedmunawwar990@gmail.com>
2015-05-24 23:55:36 -0800

    added one line

--- SalesToolsRepository/src/com/aditya/local/MoreSales.java
+++ SalesToolsRepository/src/com/aditya/local/MoreSales.java
@@ -4,5 +4,6 @@ public class MoreSales {
 
 	public void printSalesData(){
 		System.out.println("This just prints more data to be implemented");
+		System.out.println("i am joining this project")
 	}
 }
